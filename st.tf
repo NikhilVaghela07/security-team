f2913d990ca579f47ff9e0d7296ce109d6166d4f
Nikhil Vaghela <vaghelanikhil246@gmail.com>
2026-09-24 10:46:07 +0530

    Create st.tf

---
+++ st.tf
@@ -0,0 +1,20 @@
+provider "aws"{
+  region = "us-east-1"
+}
+
+data "terraform_remote_state" "network" {
+  backend = "remote"
+  config = {
+    organization = "kenilith"
+
+    workspace = {
+      name = "network-team"
+    }
+  }
+}
+
+
+output "network_public_ips" {
+  description = "Network team IPs list featched"
+  value = data.terraform_remote_state.network.outputs.public_ips
+}
